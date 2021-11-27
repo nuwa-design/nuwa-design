30c2b40a871056eccf8163b3316d11e39bbb114b
qulongjun <github@muwa.design>
2021-11-27 12:18:29 +0800

    build(changelog config file): changelog config 文件

--- changelog.config.ts
+++ changelog.config.ts
@@ -1,6 +1,21 @@
 /**
  * @FileName /changelog.config.ts
- * @Description
+ * @Description Git CZ config
  * @Author qulongjun <contact@nuwa.design>
  * @Date 2021/11/27 12:11:14
  */
+module.exports = {
+  disableEmoji: false,
+  "list": [
+    "test",
+    "feat",
+    "fix",
+    "chore",
+    "docs",
+    "refactor",
+    "style",
+    "ci",
+    "perf"
+  ],
+
+}
