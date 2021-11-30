b01f548e1c100959872ff47fa2b7778725a26f64
jonnyqu <jonnyqu@tencent.com>
2021-11-30 11:39:39 +0800

    docs(grid): update grid doc
    更新 Grid 组件文档

--- src/grid/interface.ts
+++ src/grid/interface.ts
@@ -18,16 +18,40 @@ export const BlockArr = ['inline', 'inline-block', 'block'] as const;
 export type BlockType = typeof BlockArr[number];
 
 export interface IColProps extends ICommonProps {
-  /** 当前分栏的数量 */
-  range?: Record<IType, number> | number;
-  /** 分栏的偏移量 */
+  /**
+   * @description       当前分栏的数量
+   * @default           无
+   * @type              ColData 类型，见下文
+   */
+  range: Record<IType, number> | number;
+  /**
+   * @description       分栏的偏移量
+   * @default           无
+   * @type              ColData 类型，见下文
+   */
   offset?: Record<IType, number> | number;
-  /** 分栏的排序：向左排序 */
+  /**
+   * @description       分栏的排序：向左排序
+   * @default           无
+   * @type              ColData 类型，见下文
+   */
   push?: Record<IType, number> | number;
-  /** 分栏的排序：向右排序 */
+  /**
+   * @description       分栏的排序：向右排序
+   * @default           无
+   * @type              ColData 类型，见下文
+   */
   pull?: Record<IType, number> | number;
-  /** 需要隐藏的分栏 */
+  /**
+   * @description       条件隐藏，需要隐藏的分栏，表示在当前屏幕分辨率下需要隐藏该分栏
+   * @default           无
+   * @type              数组类型，支持 分辨率规则 中一个或多个
+   */
   hidden?: IType[];
-  /** 需要可见的分栏 */
+  /**
+   * @description       条件可见，需要显示的分栏，表示在当前屏幕分辨率下需要显示该分栏，与 hidden 不同时使用
+   * @default           无
+   * @type              ColVisible 类型，见下文
+   */
   visible?: [IType, BlockType][] | IType[];
 }
